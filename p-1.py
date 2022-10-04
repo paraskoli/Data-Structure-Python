3bfb63cad65fbc96378b4cf30a3c15d740b40131
paras koli <paraskoli43@gmail.com>
2022-10-04 22:54:32 +0530

    tuple.py

---
+++ p-1.py
@@ -0,0 +1,18 @@
+# a={"name":"paras","age":"24","class":"Btech"}
+# # a.items
+# # print(a)
+
+# print(a.keys())
+
+# print(a.update("name":"sonu kabir"))
+
+# a=100
+# b=21
+# print(a//b)
+
+# a=4
+# b=2
+# print(a**b)
+
+a=int(input("enter"))
+print(a)
