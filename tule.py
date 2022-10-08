88d319a10feb84efef2882f37496930dd5f64b7e
paras koli <paraskoli43@gmail.com>
2022-10-08 20:58:42 +0530

    list.py

--- tule.py
+++ tule.py
@@ -1,9 +1,22 @@
-# sample Tuples
+# Empty Tuple
+from calendar import c
+
+
+a=()
+print(a)
+
+#Tuple with one element
+b=(1,)
+print(b)
+
+#length of tuple
+c=[8,"paras",9,6,4,34]
+print(len(c))
+
+print(type(c[1]))
+
+
 Tuple1 = ("A", 1, "B", 2, "C", 3)
-Tuple2 = ("Geek1", "Raju", "Geek2", "Nikhil", "Geek3", "Deepanshu")
-Tuple3 = ((1, "Lion"), ( 2, "Tiger"), (3, "Fox"), (4, "Wolf"))
-  
-# print the sizes of sample Tuples
-print("Size of Tuple1: " + str(sys.getsizeof(Tuple1)) + "bytes")
-print("Size of Tuple2: " + str(sys.getsizeof(Tuple2)) + "bytes")
-print("Size of Tuple3: " + str(sys.getsizeof(Tuple3)) + "bytes")
+Tuple2 = ("name1", "paras ", "name2", "sonu", "name3", "mayank")
+Tuple3 = ((1, "goat"), ( 2, "ziraf"), (3, "cat"), (4, "tiger"))
+print(Tuple1,Tuple2,Tuple3)
