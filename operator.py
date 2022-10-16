b7f67f8c0c71b760645cd9f223cc780ea1e99517
paras koli <paraskoli43@gmail.com>
2022-10-16 17:27:09 +0530

    operator.py

---
+++ operator.py
@@ -0,0 +1,29 @@
+#arithmatic operator
+print(45+24)
+print(45-24)
+print(45*24)
+print(45/24)
+
+print(45//2) #floor division
+print(55%2) #modulus gives reminder
+print(2**4) #power function(square)
+a=9
+a +=9 #(a=a+9)
+print(a)
+
+#comparison operator
+# a=9
+# b=8
+# print(a!=b)
+# print(a==b)
+# print(a>=b)
+# print(a<=b)
+# print(a>b)
+# print(a<b)
+
+#logical operator 
+a=9
+b=8
+# print(a>=8 and b<=8) #both condition are true
+print(a>=10 or b<=9) #one condition is true
+print(a!=9) # condition not equal to true
