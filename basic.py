3c91c9c672212bd7c0d1cf12cf7eec59051f871c
paras koli <paraskoli43@gmail.com>
2022-10-16 11:01:04 +0530

    basic.py

--- basic.py
+++ basic.py
@@ -1,22 +1,21 @@
-# #taking input from user
-# Birth_Year=int(input("Enter your Birth Year: "))
-# print(2022-Birth_Year)
+#taking input from user
+Birth_Year=int(input("Enter your Birth Year: "))
+print(2022-Birth_Year)
 
-# #Sum of two number
-# first=int(input("Enter a Number:"))
-# second=int(input("Enter a Number:"))
-# print(first+second)
+#Sum of two number
+first=int(input("Enter a Number:"))
+second=int(input("Enter a Number:"))
+print(first+second)
 
 #multi line input
-# name="""paras 
-#         koli """
-# print(name.upper())
-# print(name.capitalize())
-# print(name.lower())
-# print(name)
-# print(name[2:])
-# print(name[-1])
-
+name="""paras 
+        koli """
+print(name.upper())
+print(name.capitalize())
+print(name.lower())
+print(name)
+print(name[2:])
+print(name[-1])
 
 name=("paraskoli1")
 print(name[-8:])
