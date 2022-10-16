452c241d6e5da1b237128a9e6878fb6fc52d6daf
paras koli <paraskoli43@gmail.com>
2022-10-16 17:46:58 +0530

    loops.py

---
+++ loops.py
@@ -0,0 +1,11 @@
+# while loop
+count =1
+while count<=10:
+    # print("God is Everything")
+    print(count * ("."))
+    count += 1
+
+count =10
+while count !=0:
+    print(count *("."))
+    count -=1
