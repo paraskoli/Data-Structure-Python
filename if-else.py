8287bb2c471c1d76bfa85e90bb24d1837753e19d
paras koli <paraskoli43@gmail.com>
2022-10-16 17:34:34 +0530

    if-else.py

---
+++ if-else.py
@@ -0,0 +1,12 @@
+age=int(input("Enter your age \n"))
+if age:
+    print("you can vote")
+else:
+    print("you can not vote")
+
+a=9
+b=8
+if b>=9:
+    print("youe are not cute")
+else:
+    print("you are cute")
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,22 +1,5 @@
-# Empty Tuple
-# from calendar import c
-
-
-# a=()
-# print(a)
-
-# #Tuple with one element
-# b=(1,)
-# print(b)
-
-# #length of tuple
-# c=[8,"paras",9,6,4,34]
-# print(len(c))
-
-# print(type(c[1]))
-
-
-# Tuple1 = ("A", 1, "B", 2, "C", 3)
-# Tuple2 = ("name1", "paras ", "name2", "sonu", "name3", "mayank")
-# Tuple3 = ((1, "goat"), ( 2, "ziraf"), (3, "cat"), (4, "tiger"))
-# print(Tuple1,Tuple2,Tuple3)
+age=int(input("Enter your age \n"))
+# if age:
+#     print("you can vote")
+# else:
+#     print("you can not vote")
