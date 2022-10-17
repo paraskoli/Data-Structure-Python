afe7ecf0e075870a63899b57cbc8c449c9d73c67
paras koli <paraskoli43@gmail.com>
2022-10-17 20:48:34 +0530

    for-loop.py

---
+++ basic/list.py
@@ -0,0 +1,42 @@
+# # a=[45,56,67,[45,4,87],90]
+# # print(a[3])
+
+# # print(len(a))
+
+# # b=[7,8,34,67,54]
+# # print(b[-3]) 
+
+# list1=[45,5,2,6,4,8]
+# print(list)
+# list.insert(0,9)
+# print(list)
+
+# # insert an element
+# list.insert(3,[4,5,8,2,1])
+# print(list)
+
+# #append add a single element
+# list.append([7,8,9,0])
+# list.append(8)
+# print(list)
+
+# #extend add multiple event at the end of the list
+# list.extend([5,7,7,8,89,78,8,78,8,])
+# print(list)
+
+# #remove remove only one 7 
+# list.remove(7)
+# print(list)
+
+# #pop delete last element
+# list.pop(7) #delete 7 element
+# list.pop() # dlete last element
+# print(list)
+
+
+list1=[9,8,8,8,9,9,7]
+
+print(sum(list1))
+print(max(list1)) #print max number in the list
+print(min(list1)) #print min number in the list
+print(sorted(list1)) #print list in sorted order
