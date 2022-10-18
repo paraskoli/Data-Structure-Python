b6ed3a9f188bbdc8ffcac394554a9fa90ed89744
paras koli <paraskoli43@gmail.com>
2022-10-18 22:02:44 +0530

    sets.py

--- basic/sets.py
+++ basic/sets.py
@@ -1,21 +1,18 @@
-list =[7,7,6,5,4,3,2]
+list =[7,7,6,5,9,6,2]
 mySet = set(list)
 print(mySet)
 
-for item in mySet:
-    print(item)
+Set={5,4,3,6,7}
+print(Set)
+seti=[7,6,5,4,7]
+Set=set(seti)
+print(Set)
 
+Set=[5,7,5,4,3]
+print(Set)
 
-# seti=[7,6,5,4,7]
-# Set=set(seti)
-# print(Set)
+mySet.add(7,6,5)
+print(mySet)
 
-# Set=[5,7,5,4,3]
-# print(Set)
-
-# mySet.add(7,6,5)
-# print(mySet)
-
-mySet.add(10)
-mySet.update({5,4,4,3,2,6})
+mySet.update({9,6,5,4,3,1,3,2})
 print(mySet)
