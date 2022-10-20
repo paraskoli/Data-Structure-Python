9c1b089c733dcf4de8175c5b6f3573f4bac81b57
paras koli <paraskoli43@gmail.com>
2022-10-20 22:54:35 +0530

    dictionary.py

--- basic/dictinary.py
+++ basic/dictinary.py
@@ -3,8 +3,19 @@ dictionary={"name":"Paras","age":22}
 mydict=dictionary.copy()
 print(mydict)
 
+print(dictionary.items())
 print(dictionary.get("name"))
 print(dictionary.values())
 print(dictionary.keys())
 print(dictionary.update())
-print(dictionary.pop)
+print(dictionary.pop("name"))
+print(dictionary)
+
+for items in dictionary:
+    print(items)
+
+for item in dictionary.values():
+    print(item)
+
+dictionary.clear()
+print(dictionary)
