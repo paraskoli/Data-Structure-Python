dc64287869da83ca0477d2a7468cf691afadb02a
paras koli <paraskoli43@gmail.com>
2022-10-23 23:41:05 +0530

    insertion.py

---
+++ insertion.py
@@ -0,0 +1,30 @@
+# Python program for implementation of Insertion Sort
+
+def insertionSort(arr):
+
+	# Traverse through 1 to len(arr)
+	for i in range(1, len(arr)):
+
+		key = arr[i]
+
+		# Move elements of arr[0..i-1], that are
+		# greater than key, to one position ahead
+		# of their current position
+		k = i-1
+		while k >=0 and key < arr[k] :
+				arr[k+1] = arr[k]
+				k -= 1
+		arr[k+1] = key
+
+
+#sorting the array [12, 11, 13, 5, 6] using insertionSort
+arr = [18,5,6,8,9]
+insertionSort(arr)
+lst = [] #empty list to store sorted elements
+print("Sorted array is : ")
+for i in range(len(arr)):
+	lst.append(arr[i])	 #appending the elements in sorted order
+print(lst)
+
+
+# This code is contributed by Mohit Kumra
