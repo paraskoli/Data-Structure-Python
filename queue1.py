7be090fd88a3c090c03fa696f28f602b6fcce1eb
paras koli <paraskoli43@gmail.com>
2022-11-15 22:41:59 +0530

    queue.py

--- queue1.py
+++ queue1.py
@@ -8,13 +8,15 @@ class Queue:
       self.queue.insert(0,dataval)
       return True
    return False
-
-   def size(self):
-      return len(self.queue)
+# Pop method to remove element
+   def removefromq(self):
+      if len(self.queue)>0:
+         return self.queue.pop()
+      return ("No elements in Queue!")
 
 TheQueue = Queue()
 TheQueue.addtoq("Mon")
 TheQueue.addtoq("Tue")
 TheQueue.addtoq("Wed")
-print(TheQueue.size())
-
+print(TheQueue.removefromq())
+print(TheQueue.removefromq())
