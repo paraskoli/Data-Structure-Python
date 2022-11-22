d4906632903152306d76e01654252520a2b1a3b3
paras koli <paraskoli43@gmail.com>
2022-11-22 23:07:23 +0530

    graph.py

---
+++ graph.py
@@ -0,0 +1,10 @@
+# Create the dictionary with graph elements
+graph = { 
+   "a" : ["b","c"],
+   "b" : ["a", "d"],
+   "c" : ["a", "d"],
+   "d" : ["e"],
+   "e" : ["d"]
+}
+# Print the graph 		 
+print(graph)
