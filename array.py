1b82be4d8d4cb9f6a12fa24bdd5752186277bce3
paras koli <paraskoli43@gmail.com>
2022-11-24 23:05:32 +0530

    array.py

--- array.py
+++ array.py
@@ -1,4 +1,13 @@
+# from array import *
+# array1 = array('i', [50,45,54,57,58,95,65])
+# for x in (array1):
+#    print(x)
+
+
 from array import *
-array1 = array('i', [50,45,54,57,58,95,65])
-for x in (array1):
-   print(x)
+
+array1 = array('i', [10,20,30,40,50])
+
+print (array1[0])
+
+print (array1[2])
