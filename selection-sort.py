f431298be2bdf83e0a2ef2c31df87e58a4e0648b
paras koli <paraskoli43@gmail.com>
2022-11-26 22:58:35 +0530

    selection-sort.py

--- selection-sort.py
+++ selection-sort.py
@@ -2,10 +2,10 @@ def selection_sort(input_list):
    for idx in range(len(input_list)):
       min_idx = idx
       for j in range( idx +1, len(input_list)):
-         if input_list[min_idx] > input_list[j]:
-         min_idx = j
+            if input_list[min_idx] > input_list[j]:
+               min_idx = j
 # Swap the minimum value with the compared value
    input_list[idx], input_list[min_idx] = input_list[min_idx], input_list[idx]
-l = [19,2,31,45,30,11,121,27]
+l = [20,54,55,45,88,66,99,45,254]
 selection_sort(l)
 print(l)
