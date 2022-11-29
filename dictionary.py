3380a045f93bd730a5af519f95480bd9a194ea35
paras koli <paraskoli43@gmail.com>
2022-11-29 21:04:07 +0530

    dictionary.py

--- dictionary.py
+++ dictionary.py
@@ -3,3 +3,12 @@ Traceback (most recent call last):
    File "test.py", line 4, in <module>
       print "dict['Alice']: ", dict['Alice'];
 KeyError: 'Alice'
+
+#!/usr/bin/python
+
+dict = {'Name': 'Zara', 'Age': 7, 'Class': 'First'}
+dict['Age'] = 8; # update existing entry
+dict['School'] = "DPS School"; # Add new entry
+
+print "dict['Age']: ", dict['Age']
+print "dict['School']: ", dict['School']
