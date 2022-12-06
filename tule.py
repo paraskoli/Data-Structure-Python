957a970fb90971bcc93a57ca8bd357994e96a293
paras koli <paraskoli43@gmail.com>
2022-12-06 22:34:46 +0530

    tuple.py

--- tule.py
+++ tule.py
@@ -21,8 +21,11 @@
 # Tuple3 = ((1, "goat"), ( 2, "ziraf"), (3, "cat"), (4, "tiger"))
 # print(Tuple1,Tuple2,Tuple3)
 
-my_tuple=(8,8,54.545,5,45,4,54,54,,54,,85,8,5,8,6,46,6,8)
+my_tuple=(8,8,54.545,5,45,4,54,54,54,85,8,5,8,6,46,6,8)
 print(my_tuple)
 print(my_tuple[0])
 
 print(my_tuple)
+
+my_tuple1=(78,87,8,78,57,5,45,45)
+print(my_tuple1)
