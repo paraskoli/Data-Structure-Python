ff5f13e23107f07f7437ff44063adfebd828a9bf
paras koli <paraskoli43@gmail.com>
2022-12-07 22:42:20 +0530

    oop.py

---
+++ oop.py
@@ -0,0 +1,14 @@
+class <ClassName>:
+
+    <class_attribute_name> = <value>
+
+    def __init__(self,<param1>, <param2>, ...):
+        self.<attr1> = <param1>
+        self.<attr2> = <param2>
+        .
+        .
+
+    
+   def <method_name>(self, <param1>, ...):
+       <code>
+
